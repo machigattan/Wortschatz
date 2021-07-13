bb24d161713d05efa14e9d59106c90a1234bdd73
Kevin Liu <58940060+machigattan@users.noreply.github.com>
2021-07-13 18:50:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ pyshell.on('message', function (message) {
     console.log(message);
 });
 
-// end the input stream and allow the process to exit
-// pyshell.end(function (err) {
-//     if (err){
-//         throw err;
-//     };
+//end the input stream and allow the process to exit
+pyshell.end(function (err) {
+    if (err){
+        throw err;
+    };
 
-//     console.log('finished');
-// });
+    console.log('finished');
+});
 
 // LED API wird importiert
 const { toggle } = require( './led-control' );
