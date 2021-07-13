7bafc366124a3f78dcb8823897e53bf6ee716a8a
Kevin Liu <58940060+machigattan@users.noreply.github.com>
2021-07-13 19:14:14 +0200

    Update script.py

--- script.py
+++ script.py
@@ -4,10 +4,9 @@ import sys, json
 def read_in():
     lines = sys.stdin.readlines()
     # Since our input would only be having one line, parse our JSON data from that
-    return json.loads(lines)
+    return json.loads(lines[0])
 
 def main():
-    
     #get our data as an array from read_in()
     lines = read_in()
 
