887b542a6867990a11da4aa9e0769f7ea62bd65f
Kevin Liu <58940060+machigattan@users.noreply.github.com>
2021-07-13 19:39:10 +0200

    at this point idk what i am doing anymore
    trying to make script.py execute writeRfid.py
    
    also have to pass on the variable lines to writeRfid..

--- script.py
+++ script.py
@@ -10,14 +10,10 @@ def main():
     #get our data as an array from read_in()
     lines = read_in()
 
-    # Sum  of all the items in the providen array
-    # total_sum_inArray = 0
-    # for item in lines:
-    #     total_sum_inArray += item
-
     #return the sum to the output stream
     print (lines)
 
 # Start process
 if __name__ == '__main__':
     main()
+    writeRfid.write_function()
--- writeRfid.py
+++ writeRfid.py
@@ -1,39 +1,33 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import sys, json
+from __main__ import *
 
 reader = SimpleMFRC522()
 
-#Read data from stdin
-def read_in():
-    lines = sys.stdin.readlines()
-
-    return json.loads(lines[0])
-
-def main():
-    #get our data as an array from read_in()
-    lines = read_in()
-
-    #return the sum to the output stream
-    print (lines) 
-    try:
-        text = lines
-        print("waiting for tag")
-        reader.write(text)
-    
-        id, text = reader.read() 
-        print("Written on ", id)
-        print(text, "is the new word")
-    finally:
-        GPIO.cleanup()
-
-
 # https://pimylifeup.com/raspberry-pi-rfid-rc522/ simple script from pimylifeup that can write on tags
 
- 
+def write_function():
+    print(lines)
+    text = lines
+    print("waiting for tag")
+    reader.write(text)
+  
+    id, text = reader.read() 
+    print("Written on ", id)
+    print(text, "is the new word")
+    GPIO.cleanup()
 
 
+# try:
+#     print(lines)
+#     text = lines
+#     print("waiting for tag")
+#     reader.write(text)
+  
+#     id, text = reader.read() 
+#      print("Written on ", id)
+#     print(text, "is the new word")
 
-# Start process
-if __name__ == '__main__':
-    main()
+# finally:
+#     GPIO.cleanup()
+
