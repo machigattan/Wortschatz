8b7cbcffa98b94ec871eeaa9ab3a30d339731644
Kevin Liu <58940060+machigattan@users.noreply.github.com>
2021-07-13 19:13:35 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,16 +1,15 @@
 import sys, json
 
 #Read data from stdin
-# def read_in():
-#     lines = sys.stdin.readlines()
-#     # Since our input would only be having one line, parse our JSON data from that
-#     return json.loads(lines)
+def read_in():
+    lines = sys.stdin.readlines()
+    # Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines)
 
 def main():
-
-        lines = sys.stdin.readlines()
+    
     #get our data as an array from read_in()
-    # lines = read_in()
+    lines = read_in()
 
     # Sum  of all the items in the providen array
     # total_sum_inArray = 0
@@ -18,7 +17,7 @@ def main():
     #     total_sum_inArray += item
 
     #return the sum to the output stream
-    print (json.loads(lines))
+    print (lines)
 
 # Start process
 if __name__ == '__main__':
