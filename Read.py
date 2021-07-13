f5cc2b99bab6785248646b4bc1711811a4b88591
Kevin Liu <58940060+machigattan@users.noreply.github.com>
2021-07-14 00:49:25 +0200

    Update Read.py

--- Read.py
+++ Read.py
@@ -4,8 +4,9 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
-GPIO.setwarnings(False)
+
 try:
+    GPIO.setwarnings(False)
     id, text = reader.read()
     print(id)
     print(text)
