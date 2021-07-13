7f051393a71d35ecae665b9037e95417d06c4d63
Kevin Liu <58940060+machigattan@users.noreply.github.com>
2021-07-13 23:20:58 +0200

    Quick Test if the variable lines is being transferred
    Trying to import lines from script.py to writeRfid

--- writeRfid.py
+++ writeRfid.py
@@ -1,20 +1,20 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-# from __main__ import *
+from __main__ import *
 
 reader = SimpleMFRC522()
 
 # https://pimylifeup.com/raspberry-pi-rfid-rc522/ simple script from pimylifeup that can write on tags
 
 def write_function():
-    # print(lines)
-    text = input('New data:')
-    print("waiting for tag")
-    reader.write(text)
+    print(lines)
+    # text = input('New data:')
+    # print("waiting for tag")
+    # reader.write(text)
   
-    id, text = reader.read() 
-    print("Written on ", id)
-    print(text, "is the new word")
+    # id, text = reader.read() 
+    # print("Written on ", id)
+    # print(text, "is the new word")
     # GPIO.cleanup()
 
 if __name__ == '__main__':
