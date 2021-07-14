79227222924f58bc5b49e5dbdc117ddd3f79f582
Kevin Liu <58940060+machigattan@users.noreply.github.com>
2021-07-14 10:27:18 +0200

    Update testscript.py

--- testscript.py
+++ testscript.py
@@ -2,7 +2,12 @@ import sys
 
 new_word = sys.argv[1]
 
-def send_it():
-    print(str(new_word))
-    return new_word
-    
+# def send():
+#     some_list = []
+#     for i in 'string':
+#          some_list.append( i )
+#     return new_word
+
+for n, a in enumerate(sys.argv):
+    # print('arg {} has value {} endOfArg'.format(n, a))
+    print("Hello and welcome " + str(new_word) + "!")
