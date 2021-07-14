40c1abaae5ac6c547f645336da540f617c55ef7e
Kevin Liu <58940060+machigattan@users.noreply.github.com>
2021-07-14 22:45:09 +0200

    Update read-after-writeRfid.py

--- read-after-writeRfid.py
+++ read-after-writeRfid.py
@@ -1,7 +1,8 @@
 # https://pimylifeup.com/raspberry-pi-rfid-rc522/ simple script from pimylifeup that can read tags
 
-GPIO.setwarnings(False)
+
 import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
